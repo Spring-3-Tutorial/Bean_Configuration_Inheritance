35a8362a53ae3c9c200181193b1d28f7bfb5aa58
kemalsami <kemalsami.karaca@tubitak.gov.tr>
2016-09-05 14:51:46 +0300

    ** Spring 3 ** Bean -> Configuration Inheritance Examples v1.02

--- src/main/java/com/guppy/spring3beans/configurationInheritance/SpringApp.java
+++ src/main/java/com/guppy/spring3beans/configurationInheritance/SpringApp.java
@@ -18,9 +18,22 @@ public class SpringApp {
         ApplicationContext context = 
                 new ClassPathXmlApplicationContext(new String[]{"Beans.xml"});
         
-        // --- Configuration Inheritance Example --- 
+        // --- Configuration Inheritance Example --- (not abstract parent Bean)
+        Customer parentObj = (Customer)context.getBean("parentBean");
+        parentObj.printCustomerInfo();
+        
+        // --- Configuration Inheritance Example --- (abstract parent Bean)
+        // --- This will occur run time error because of abstraction
+        //Customer abstractParentObj = (Customer)context.getBean("abstractParentBean");
+        //abstractParentObj.printCustomerInfo();
+        
+        // --- Configuration Inheritance Example --- (child Bean)
         Customer childObj = (Customer)context.getBean("childBean");
         childObj.printCustomerInfo();
+        
+        // --- Configuration Inheritance Override Example --- 
+        Customer childObjTwo = (Customer)context.getBean("childBeanTwo");
+        childObjTwo.printCustomerInfo();
         
         
     }
